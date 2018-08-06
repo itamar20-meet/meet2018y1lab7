6eecea0deda969f032b423592095dcf96597d113
itamar20-meet <itamar20@meet.mit.edu>
2018-08-06 07:32:22 -0400

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,11 @@
+def add_numbers(start, end):
+    
+    product= int((start)=start+1)*(start,end)
+    return product
+
+test1 = add_numbers(1,2)
+print(test1)
+test2 = add_numbers(1, 100)
+print(test2)
+test3 = add_numbers(1000, 5000)
+print(test3) 
